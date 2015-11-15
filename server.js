85e47d64800faf138e543d368b0123df62142eb4
D <dmitlyan@mit.edu>
2015-11-15 15:40:53 -0800

    my third check-in

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http = require("http");
 http.createServer(function(request, response) {
    response.writeHead(200,  {"Content-Type": "text/plain"});
    response.write("Hellow World!!!");
-   resonse.end();
+   response.end();
 }).listen(process.env.PORT || 8888);
 
